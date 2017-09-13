533cecc9d08ecea590b3d2030c56fa085a7b2968
Reed Williams <reed.williams@siemens.com>
2017-09-13 14:58:04 -0400

    Adding WYWOT!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shorties
 A repo to hold little bitty projects that don't need a whole repo apiece.
+
+-----
+
+=WYWOT=
+
+A python script that you call with the names of up to six projects or tasks that you want to indicate you're working on, either to yourself or to others. You print the resulting webpage and fold it into a little triangular prism that sits on your desk and can be rotated to show the project you're working on! 
---
+++ WYWOT/WYWOT.py
@@ -0,0 +1,49 @@
+# python script for generating What Ya Working On? trifolds
+# Supports up to six projects.
+# 
+# Usage: call from command line
+#    python WYWOT.py "project name 1" "project name 2" ... "project name 6"
+# then print the resulting webpage. 
+# Next, fold the sheet in half, then fold each half in half. Lastly, 
+# tuck the blank quarter behind the top quarter and tape! 
+ 
+import os
+import sys
+import webbrowser
+
+def WYWOT(*args):
+    proj = []
+    for i in range(6):
+        try:
+            proj.append(args[i])
+        except: 
+            proj.append(' ')
+
+    html = '<!DOCTYPE html>\
+    <html>\
+    <body>\
+    \
+    <svg height="900" width="800" xmlns:xlink="http://www.w3.org/1999/xlink">\
+    <text x="50" y="90" fill="blue" font-family="Verdana" font-size="78">%s</text>\
+    <text x="0" y="-60" fill="red" font-family="Verdana" font-size="78" transform="rotate(180 380,30)">%s</text>\
+    <text x="50" y="360" fill="green" font-family="Verdana" font-size="78">%s</text>\
+    <text x="0" y="-330" fill="blue" font-family="Verdana" font-size="78" transform="rotate(180 380,30)">%s</text>\
+    <text x="50" y="630" fill="red" font-family="Verdana" font-size="78">%s</text>\
+    <text x="0" y="-600" fill="green" font-family="Verdana" font-size="78" transform="rotate(180 380,30)">%s</text>\
+    Sorry, your browser does not support inline SVG.\
+    </svg>\
+    </body>\
+    </html>' % (proj[0], proj[1], proj[2], proj[3], proj[4], proj[5])
+
+    path = os.path.abspath('wywot.html')
+    url = 'file://' + path
+
+    with open(path, 'w') as f:
+        f.write(html)
+        webbrowser.open(url)
+
+callwith = []
+for i in range(1,len(sys.argv)):
+    callwith.append(sys.argv[i])
+
+WYWOT(*callwith)
